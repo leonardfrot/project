7f755609432fb75b1d43bdf4dfcce490b4b1eda2
leonardfrot <leonard.frote@he-arc.ch>
2021-12-19 17:28:58 +0100

    formulaire de base

--- lib/ui/add_note_page.dart
+++ lib/ui/add_note_page.dart
@@ -3,43 +3,52 @@ import 'package:flutter/material.dart';
 import 'package:project/services/theme_service.dart';
 import 'package:get/get.dart';
 
-class AddNotePage extends StatelessWidget {
+class AddNotePage extends StatefulWidget {
   const AddNotePage({Key? key}) : super(key: key);
 
+  @override
+  State<AddNotePage> createState() => _AddNotePageState();
+}
+
+class _AddNotePageState extends State<AddNotePage> {
+  DateTime _selectedDate = DateTime.now();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: _appBar(),
         body: Container(margin: EdgeInsets.symmetric(horizontal: 20),
-        child: Column(children: [
+        
+          child: Column(children: [
           Container(
               decoration: BoxDecoration(border: Border.all()),
-              child: const TextField(
+              child:  TextFormField(
                   decoration: InputDecoration(hintText: 'Titre'))),
                   SizedBox(height: 10,),
           Expanded(
             child: Container(
                 decoration: BoxDecoration(border: Border.all()),
-                child: const TextField(
+                child: TextFormField(
                   maxLines: null,
                   expands : true,
                     decoration: InputDecoration(hintText: 'Taper le texte ici'))),
           ),
           SizedBox(height: 10,),
-           Container(
-              decoration: BoxDecoration(border: Border.all()),
-              child: const TextField(
-                  decoration: InputDecoration(hintText: 'Autres'))),
+
+          Container(
+            decoration: BoxDecoration(border: Border.all()),
+              child:  TextFormField(
+                  readOnly: widget ==null?false: true,
+                  decoration: InputDecoration(hintText: 'Titre'))),
                   SizedBox(height: 10,),
-        ])
+
+          
+           
+                  
+        ]),
         
-        )
-        
-        
-        
-        
-        
+        ),
         );
+        
   }
 
   _appBar() {
