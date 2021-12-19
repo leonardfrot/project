90caceafbb42967d8f619f6278f3fe7ba7978076
leonardfrot <leonard.frote@he-arc.ch>
2021-12-19 04:02:34 +0100

    page de création

--- lib/ui/add_note_page.dart
+++ lib/ui/add_note_page.dart
@@ -4,42 +4,64 @@ import 'package:project/services/theme_service.dart';
 import 'package:get/get.dart';
 
 class AddNotePage extends StatelessWidget {
-  const AddNotePage({ Key? key }) : super(key: key);
+  const AddNotePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: _appBar(),
-      body: Column(
-        children:[
-          Container(decoration: BoxDecoration(border:Border.all()),
-          child: TextField(
-            decoration: InputDecoration(hintText: 'Titre'))
-          )
-        ]
-      )
-      
-    );
+        appBar: _appBar(),
+        body: Container(margin: EdgeInsets.symmetric(horizontal: 20),
+        child: Column(children: [
+          Container(
+              decoration: BoxDecoration(border: Border.all()),
+              child: const TextField(
+                  decoration: InputDecoration(hintText: 'Titre'))),
+                  SizedBox(height: 10,),
+          Expanded(
+            child: Container(
+                decoration: BoxDecoration(border: Border.all()),
+                child: const TextField(
+                  maxLines: null,
+                  expands : true,
+                    decoration: InputDecoration(hintText: 'Taper le texte ici'))),
+          ),
+          SizedBox(height: 10,),
+           Container(
+              decoration: BoxDecoration(border: Border.all()),
+              child: const TextField(
+                  decoration: InputDecoration(hintText: 'Autres'))),
+                  SizedBox(height: 10,),
+        ])
+        
+        )
+        
+        
+        
+        
+        
+        );
   }
 
-   _appBar(){
+  _appBar() {
     return AppBar(
       leading: GestureDetector(
-        onTap:(){
+        onTap: () {
           Get.back();
-          
         },
-        child: const Icon(Icons.arrow_back, size: 20, ),
+        child: const Icon(
+          Icons.arrow_back,
+          size: 20,
         ),
-
-        actions:const [
-          Icon(Icons.person, size: 20, ),
-
-          SizedBox(width: 20, ), 
-        
-        ],
-      );
-   }
-   
-   
-   }
+      ),
+      actions: const [
+        Icon(
+          Icons.person,
+          size: 20,
+        ),
+        SizedBox(
+          width: 20,
+        ),
+      ],
+    );
+  }
+}
